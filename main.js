5878675db0310f9927c8ab02fe317106699c383e
RafaelMarquesO1 <rafaelmarquesm.0209@gmail.com>
2024-03-13 22:43:30 -0300

    Primeiras alterações/criações.

---
+++ main.js
@@ -0,0 +1,4 @@
+// Adicione ou substitua seu código JavaScript pelo seguinte:
+document.querySelector('.toggle-menu').addEventListener('click', function() {
+    document.querySelector('.menu-links').classList.toggle('active');
+});
